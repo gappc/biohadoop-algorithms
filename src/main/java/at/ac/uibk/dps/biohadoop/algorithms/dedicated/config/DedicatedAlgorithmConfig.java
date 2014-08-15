daec1e2a63b3878caca2555ee3ac564ced441e9a
Christian Gapp <gapp.christian@gmail.com>
2014-08-15 04:40:31 +0200

    Removed unused config files

--- src/main/java/at/ac/uibk/dps/biohadoop/algorithms/dedicated/config/DedicatedAlgorithmConfig.java
+++
@@ -1,6 +0,0 @@
-package at.ac.uibk.dps.biohadoop.algorithms.dedicated.config;
-
-import at.ac.uibk.dps.biohadoop.algorithm.AlgorithmConfiguration;
-
-public class DedicatedAlgorithmConfig implements AlgorithmConfiguration {
-}
