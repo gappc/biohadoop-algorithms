e29ff58131c38857afe8181a515b323adac05ecd
Christian Gapp <gapp.christian@gmail.com>
2014-08-29 14:41:42 +0200

    Removed unused import

--- src/main/java/at/ac/uibk/dps/biohadoop/algorithms/typetest/remote/complexobject/ComplexObject.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/algorithms/typetest/remote/complexobject/ComplexObject.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import at.ac.uibk.dps.biohadoop.communication.master.kryo.KryoObjectRegistration;
-
 public class ComplexObject implements Serializable {
 
 	private static final long serialVersionUID = -6185201906103139533L;
