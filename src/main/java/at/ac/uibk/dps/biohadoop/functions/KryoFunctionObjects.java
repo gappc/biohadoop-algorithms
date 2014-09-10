38cf99d3578936851efb1cea1a7fd244ed9c8a08
Christian Gapp <gapp.christian@gmail.com>
2014-09-10 04:19:47 +0200

    Added kryo definition for ZDT* functions

---
+++ src/main/java/at/ac/uibk/dps/biohadoop/functions/KryoFunctionObjects.java
@@ -0,0 +1,30 @@
+package at.ac.uibk.dps.biohadoop.functions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import at.ac.uibk.dps.biohadoop.utils.KryoRegistrator;
+
+import com.esotericsoftware.kryo.Serializer;
+
+public class KryoFunctionObjects implements KryoRegistrator{
+
+	@Override
+	public List<Class<? extends Object>> getRegistrationObjects() {
+		List<Class<? extends Object>> result = new ArrayList<Class<? extends Object>>();
+		result.add(Zdt1.class);
+		result.add(Zdt2.class);
+		result.add(Zdt3.class);
+		result.add(Zdt4.class);
+		result.add(Zdt6.class);
+		return result;
+	}
+
+	@Override
+	public Map<Class<? extends Object>, Serializer<?>> getRegistrationObjectsWithSerializer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
