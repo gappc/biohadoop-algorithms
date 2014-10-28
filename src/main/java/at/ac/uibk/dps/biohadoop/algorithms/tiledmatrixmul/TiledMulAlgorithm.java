e459e2d75f7e83c15149ba8d7a117f0d79c09458
Christian Gapp <gapp.christian@gmail.com>
2014-10-28 19:09:04 +0100

    Works now with different tile sizes for x and y matrix dimensions. SBX is used for crossover, Parameter based mutator is used for muation

--- src/main/java/at/ac/uibk/dps/biohadoop/algorithms/tiledmatrixmul/TiledMulAlgorithm.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/algorithms/tiledmatrixmul/TiledMulAlgorithm.java
@@ -22,9 +22,12 @@ public class TiledMulAlgorithm implements Algorithm {
 
 	public static final String POP_SIZE = "POP_SIZE";
 	public static final String MUTATION_FACTOR = "MUTATION_FACTOR";
+	public static final String MATRIX_LAYOUT = "MATRIX_LAYOUT";
 	public static final String MATRIX_SIZE = "MATRIX_SIZE";
 	public static final String MAX_BLOCK_SIZE = "MAX_BLOCK_SIZE";
 	public static final String ITERATIONS = "ITERATIONS";
+	public static final String LAYOUT_ROW = "LAYOUT_ROW";
+	public static final String LAYOUT_COL = "LAYOUT_COL";
 
 	private static final Logger LOG = LoggerFactory
 			.getLogger(TiledMulAlgorithm.class);
@@ -37,7 +40,7 @@ public class TiledMulAlgorithm implements Algorithm {
 
 		// Save current population and new new child population as one array.
 		// First the current population, then the new child population
-		final int[] population = generatePopulation(prep);
+		final int[][] population = generatePopulation(prep);
 		final long[] fitness = new long[2 * prep.getPopSize()];
 
 		final List<TaskFuture<Long>> futures = new ArrayList<TaskFuture<Long>>();
@@ -54,20 +57,22 @@ public class TiledMulAlgorithm implements Algorithm {
 		}
 
 		int count = 0;
-		int blockMax = 0;
-		int blockMin = prep.getMaxBlockSize();
+		int[] blockMax = new int[2];
+		int[] blockMin = new int[] { prep.getMaxBlockSize(),
+				prep.getMaxBlockSize() };
 		long iterationStart = System.nanoTime();
 		while (count < prep.getIterations()) {
 			long start = System.nanoTime();
 
 			futures.clear();
 			for (int i = prep.getPopSize(); i < 2 * prep.getPopSize(); i++) {
-				population[i] = recombinePopulation(population, fitness,
+				int[] child = recombinePopulation(population, fitness,
 						prep.getMaxBlockSize());
-				population[i] = mutatePopulation(population[i],
+				int[] mutatedChild = mutateChild(child,
 						prep.getMutationFactor(), prep.getMaxBlockSize());
+				population[i] = mutatedChild;
 			}
-			
+
 			long remoteStart = System.nanoTime();
 			for (int i = prep.getPopSize(); i < 2 * prep.getPopSize(); i++) {
 				futures.add(submitFitnessComputation(prep.getTaskSubmitter(),
@@ -79,8 +84,7 @@ public class TiledMulAlgorithm implements Algorithm {
 			long remoteEnd = System.nanoTime();
 
 			LOG.info("Before sort");
-			LOG.info(Arrays.toString(population));
-			LOG.info(Arrays.toString(fitness));
+			logIterationResult(population, fitness);
 
 			// Simple Bubble-Sort to sort best individuals to the front
 			for (int i = 0; i < 2 * prep.getPopSize(); i++) {
@@ -90,7 +94,7 @@ public class TiledMulAlgorithm implements Algorithm {
 						fitness[i] = fitness[j];
 						fitness[j] = tmpFitness;
 
-						int individual = population[i];
+						int[] individual = population[i];
 						population[i] = population[j];
 						population[j] = individual;
 					}
@@ -98,19 +102,24 @@ public class TiledMulAlgorithm implements Algorithm {
 			}
 
 			LOG.info("After sort");
-			LOG.info(Arrays.toString(population));
-			LOG.info(Arrays.toString(fitness));
+			logIterationResult(population, fitness);
 
 			// Compute global maximum and minimum blocksize, just for interest
 			for (int i = 0; i < 2 * prep.getPopSize(); i++) {
-				blockMin = Math.min(population[i], blockMin);
-				blockMax = Math.max(population[i], blockMax);
+				for (int j = 0; j < 2; j++) {
+					blockMin[j] = Math.min(population[i][j], blockMin[j]);
+					blockMax[j] = Math.max(population[i][j], blockMax[j]);
+				}
 			}
 
 			long end = System.nanoTime();
 
-			LOG.info("iteration={}, blockMin={}, blockMax={}, iteration-time={}ns, time_seq={}ns, time-par={}ns",
-					count++, blockMin, blockMax, (end - start), (end - start) - (remoteEnd - remoteStart), (remoteEnd - remoteStart));
+			LOG.info(
+					"iteration={}, blockMin={}, blockMax={}, iteration-time={}ns, time_seq={}ns, time-par={}ns",
+					count++, Arrays.toString(blockMin),
+					Arrays.toString(blockMax), (end - start), (end - start)
+							- (remoteEnd - remoteStart),
+					(remoteEnd - remoteStart));
 
 			ProgressService.setProgress(solverId,
 					(float) count / prep.getIterations());
@@ -134,26 +143,29 @@ public class TiledMulAlgorithm implements Algorithm {
 		LOG.info("-----------------------");
 	}
 
-	private int[] generatePopulation(Preparation prep) {
+	private int[][] generatePopulation(Preparation prep) {
 		final Random rand = new Random();
-		final int[] population = new int[2 * prep.getPopSize()];
+		final int[][] population = new int[2 * prep.getPopSize()][2];
 		// We only need to initialize current population, set new population to
 		// 0
 		for (int i = 0; i < prep.getPopSize(); i++) {
-			// Set blockSize. As a blockSize of 0 would make no sense, we add 1
-			population[i] = rand.nextInt(prep.getMaxBlockSize()) + 1;
+			for (int j = 0; j < 2; j++) {
+				// Set blockSize. As a blockSize of 0 would make no sense, we
+				// add 1
+				population[i][j] = rand.nextInt(prep.getMaxBlockSize()) + 1;
+			}
 		}
 		return population;
 	}
 
 	private TaskFuture<Long> submitFitnessComputation(
-			TaskSubmitter<Integer, Long> taskSubmitter, int blockSize)
+			TaskSubmitter<int[], Long> taskSubmitter, int[] blocks)
 			throws TaskException {
-		return taskSubmitter.add(blockSize);
+		return taskSubmitter.add(blocks);
 	}
 
-	// Using k-tournament with k = 2
-	private int recombinePopulation(int[] population, long[] fitness,
+	// Using k-tournament with k = 2 for parent selection and SBX for crossover
+	private int[] recombinePopulation(int[][] population, long[] fitness,
 			int maxBlockSize) {
 		final Random rand = new Random();
 		int parents[] = new int[2];
@@ -170,30 +182,39 @@ public class TiledMulAlgorithm implements Algorithm {
 			}
 		}
 
-		int[] children = SBX.bounded(1, population[parents[0]],
-				population[parents[1]], 1, maxBlockSize);
-		if (rand.nextBoolean()) {
-			return children[0];
-		} else {
-			return children[1];
+		int[] result = new int[2];
+		for (int i = 0; i < 2; i++) {
+			int[] children = SBX.bounded(1, population[parents[0]][i],
+					population[parents[1]][i], 1, maxBlockSize);
+			// Each child has 50% probability to get chosen
+			if (rand.nextDouble() < 0.5) {
+				result[i] = children[0];
+			} else {
+				result[i] = children[1];
+			}
 		}
+		return result;
 	}
 
-	private int mutatePopulation(int individual, int mutationFactor,
+	private int[] mutateChild(int[] child, double mutationFactor,
 			int maxBlockSize) {
-		final Random rand = new Random();
-		// Generate new value between -MUTATION_FACTOR (inclusive) and
-		// +MUTATION_FACTOR (inclusive)
-		int mutation = rand.nextInt(2 * mutationFactor + 1) - mutationFactor;
+		int[] result = new int[2];
+		for (int i = 0; i < 2; i++) {
+			result[i] = ParamterBasedMutator.mutate(child[i], mutationFactor,
+					1, maxBlockSize);
+		}
+		return result;
+	}
 
-		int newIndividual = individual + mutation;
-		if (newIndividual < 1) {
-			return 1;
+	private void logIterationResult(int[][] population, long[] fitness) {
+		StringBuilder sb = new StringBuilder("[");
+		for (int i = 0; i < population.length; i++) {
+			sb.append(Arrays.toString(population[i])).append(", ");
 		}
-		if (newIndividual > maxBlockSize) {
-			return maxBlockSize;
-		}
-		return newIndividual;
+		sb.deleteCharAt(sb.length() - 1);
+		sb.append("]");
+		LOG.info(sb.toString());
+		LOG.info(Arrays.toString(fitness));
 	}
 
 }
